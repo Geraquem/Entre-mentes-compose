b89f3b88e57d4d63cfff2364f12aad5d06b00929
gers <geraquem@gmail.com>
2026-09-06 19:46:33 +0200

    wip

--- app/src/main/java/com/mmfsin/betweenminds/presentation/MainActivity.kt
+++ app/src/main/java/com/mmfsin/betweenminds/presentation/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.mmfsin.betweenminds.presentation.core.navigation.NavigationMain
 import com.mmfsin.betweenminds.presentation.core.theme.BMCTheme
 import com.mmfsin.betweenminds.utils.NAV_QUESTIONS_OFFLINE
@@ -15,9 +18,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { BMCTheme { NavigationMain() } }
 
-        //        this.openBedRockActivity(NAV_QUESTIONS_ONLINE)
-//                this.openBedRockActivity(NAV_QUESTIONS_OFFLINE)
+        immersiveMode()
+
+        setContent { BMCTheme { NavigationMain() } }
+    }
+
+    private fun immersiveMode() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
     }
 }
